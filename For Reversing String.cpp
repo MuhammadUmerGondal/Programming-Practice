955252fd603992309833c2c2abbd2b4ebb26e10c
Muhammad Umar <116914394+MuhammadUmerGondal@users.noreply.github.com>
2023-06-10 02:19:13 +0500

    Add files via upload

---
+++ For Reversing String.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string myName = "Muhammad Umer";
+    for(int i = myName.length(); i>0; i--){
+        cout << myName[i];
+
+    }
+
+}
